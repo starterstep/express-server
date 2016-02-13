750d0a074535852d0b60b432f2403c011ec8db7d
Brian Knorr <brianknorr@BRIANs-MBP.fios-router.home>
2016-02-13 13:35:33 -0600

    small change

--- example/settings/index.js
+++ example/settings/index.js
@@ -5,7 +5,6 @@ if (process.env.NODE_ENV === 'development') {
     require('longjohn');
 }
 
-var path = require('path');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
